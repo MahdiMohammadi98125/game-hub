43dad0dd3c78f913b5eac12e3c80f818bfb4a494
MahdiMohammadi98125 <mahdi012021@gmail.com>
2023-10-30 20:37:12 +0430

    create a custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,10 @@
-import { useState, useEffect } from "react";
-import apiClient from "./services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "./hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => {
-        setGames(res.data.results);
-      })
-      .catch((err) => setError(err.message));
-  }, []);
-
+  const { games, error } = useGames();
   return (
     <>
-      {error && <p>unable to load the games!</p>}
+      {error && <p>{error}</p>}
       <ul>
         {games?.map((game) => (
           <li key={game.id}>{game.name}</li>
