13f125c2e5ec03096cb4eed6bb0ae6f108172e90
MahdiMohammadi98125 <mahdi012021@gmail.com>
2023-10-31 15:27:22 +0430

    displaying the genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="250px" borderRadius="10" overflow="hidden">
+    <Box borderRadius="10" overflow="hidden">
       {children}
     </Box>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,8 +11,8 @@ const GameGrid = () => {
     <>
       {error && <p>{error}</p>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing="10"
+        columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
+        spacing="5"
         padding="10px"
       >
         {isLoading &&
